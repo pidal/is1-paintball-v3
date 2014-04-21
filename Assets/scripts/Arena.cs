21c25e3dc6bfc564291314df36d1055c2fef655f
Alfonso Pidal Ligués <alfonsopidal@gmail.com>
2014-04-21 23:48:37 +0200

    modificada la scene, añadida torre de obstaculo

--- Assets/scripts/Arena.cs
+++ Assets/scripts/Arena.cs
@@ -5,7 +5,7 @@ public class Arena : Terreno {
 
 	// Use this for initialization
 	void Start () {
-		cambio_velocidad = 0.9f;
+		cambio_velocidad = 0.8f;
 		cambio_radio_vision = 1;
 		cambio_radio_disparo = 1;
 		cambio_radio_deteccion = 1;
